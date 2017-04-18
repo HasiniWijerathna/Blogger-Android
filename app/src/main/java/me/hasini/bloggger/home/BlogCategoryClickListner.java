afe7751088ceedb083b83ce5c341497a0aaeaac2
HasiniWijerathna <hasini@calcey.com>
2017-04-18 13:33:02 +0530

    Added a blog category click listner interface

---
+++ app/src/main/java/me/hasini/bloggger/home/BlogCategoryClickListner.java
@@ -0,0 +1,11 @@
+package me.hasini.bloggger.home;
+
+import me.hasini.bloggger.lib.models.BlogCategory;
+
+/**
+ * Created by Calcey on 07-Apr-17.
+ */
+
+public interface BlogCategoryClickListner  {
+    void onClickBlogCategory(BlogCategory blogCategory);
+}
