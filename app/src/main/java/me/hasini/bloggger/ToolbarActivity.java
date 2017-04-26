c2274cac861399702571814885ceed90b6096d25
HasiniWijerathna <hasini@calcey.com>
2017-04-26 14:09:03 +0530

    Added a toolbar component

---
+++ app/src/main/java/me/hasini/bloggger/ToolbarActivity.java
@@ -0,0 +1,15 @@
+package me.hasini.bloggger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.hasini.bloggger.R;
+
+public class ToolbarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+    }
+}
